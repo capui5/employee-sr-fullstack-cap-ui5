cc21482e5120d3be58209dac28703455c871b068
capui5 <aarinitest@gmail.com>
2023-07-27 10:14:11 +0000

    first commit

---
+++ app/empreg/webapp/controller/View2.controller.js
@@ -0,0 +1,29 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, Filter, FilterOperator) {
+        "use strict";
+
+        return Controller.extend("empreg.controller.View2", {
+            onInit: function () {
+                this.oTable = this.byId("table0");
+            },
+            onFilter: function (oEvent) {
+                // build filter array
+                var aFilter = [];
+                var sQuery = oEvent.getParameter("query");
+                if (sQuery) {
+                    aFilter.push(new Filter("ID", FilterOperator.EQ, parseInt(sQuery)));
+                }
+                // filter binding
+                var oList = this.byId("table0");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilter);
+            }
+        });
+    });
